531335b1f25dbec948d0d05fa2a8eb06db347b46
Fabian Jankowski <fjankowsk@gmail.com>
2021-11-22 15:31:40 +0000

    Reformat using black code formatter.

--- NE2001_web_output_broken.py
+++ NE2001_web_output_broken.py
@@ -3,8 +3,8 @@
 import matplotlib.pyplot as plt
 
 freqs = [0.728, 1.0, 1.4, 3.1]
-web = [6.18E3, 9.81E3, 0.03E6, 10.92E6]
-real = [2.42E3, 9.81E3, 43.12E3, 1.42E6]
+web = [6.18e3, 9.81e3, 0.03e6, 10.92e6]
+real = [2.42e3, 9.81e3, 43.12e3, 1.42e6]
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
@@ -16,11 +16,9 @@ ax.set_xscale("log")
 ax.grid()
 ax.set_xlabel("Centre frequency [GHz]")
 ax.set_ylabel(r"$\Delta \nu_\mathrm{DISS}$ [Hz]")
-ax.set_ylim(2.3E3, 11E6)
+ax.set_ylim(2.3e3, 11e6)
 ax.legend(loc="upper left")
 
-plt.savefig("NE2001_comparison.png",
-dpi=200,
-bbox_inches="tight")
+plt.savefig("NE2001_comparison.png", dpi=200, bbox_inches="tight")
 
 plt.show()
